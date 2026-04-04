fcb15b286b3b815e945e67752dd66531e0e8d079
Steve <zltstl888@gmail.com>
2026-04-05 05:28:15 +0800

    fix: mobile upload box indent via CSS media query

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -113,7 +113,7 @@ export default function UploadPage({ onParsed }: Props) {
           initial={{ opacity: 0, scale: 0.96 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.55, delay: 0.2, ease: [0.22, 1, 0.36, 1] }}
-          className="px-3 sm:px-0"
+          className="upload-zone-mobile-indent"
           style={{ width: '100%', marginBottom: 28 }}
         >
           <AnimatePresence mode="wait">
